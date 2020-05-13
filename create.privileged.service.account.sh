89ff805601ec7d303b363d9059095151d66499d7
Amol Deshmukh <a.deshmukh@northeastern.edu>
2020-05-13 00:51:25 -0400

    it is working

--- create.privileged.service.account.sh
+++ create.privileged.service.account.sh
@@ -1,2 +1,4 @@
 # oc create serviceaccount -n ci-cd privilegeduser
 oc adm policy add-scc-to-user privileged -n ci-cd -z privilegeduser
+oc policy -n ci-cd add-role-to-user view system:serviceaccount:ci-cd:privilegeduser
+oc adm groups new sonar-administrators a.deshmukh 
